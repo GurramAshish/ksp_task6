f4027bab8619bc594376dbc46c54b35fda2ed284
GurramAshish <141108968+GurramAshish@users.noreply.github.com>
2023-12-12 22:33:10 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,44 @@
+print("Program Starts!!")
+
+#variable
+num=10
+print(num)
+
+#operators
+num1=10
+num2=20
+print(num1+num2)
+print(num1*num2)
+print(num1/num2)
+print(num1-num2)
+print(num1%num2)
+print(num1**num2)
+print("the above are arthematic operations")
+
+print(num1==num2)
+print(num1<=num2)
+print(num1>=num2)
+print(num1!=num2)
+print(num1<num2)
+print(num1>num2)
+print(num1==num2)
+print("the above are comparartion operators")
+
+#functions
+def ashish(num1,num2):
+    return num1+num2
+print(ashish(100,200))
+
+#conditional statements
+num3=10
+if(num3==40):
+    print("true")
+else:
+    print("Wrong")
+
+
+#looping statements
+num=0
+while num<=5:
+    print("Ashish")
+    num=num+1
---
+++ script.js
@@ -0,0 +1,41 @@
+console.log("Hello!! world");
+
+
+
+// functiion example
+function A(){
+    console.log("singing");
+}
+
+function B(){
+    console.log("Dancing");
+}
+
+B();
+
+function subtraction(num1,num2){
+    return num1+num2
+}
+console.log (subtraction(100,200));
+
+// Conditional statements
+let num=10;
+if(num==10){
+    console.log("correct!!");
+}
+else{
+    console.log("Worng!!");
+}
+
+// looping statements
+let ash;
+
+for(ash=0;ash<50;ash++){
+    console.log("Ashish")
+}
+
+let ashish=0;
+
+while(ashish<10){
+    console.log("ashish"+ashish)
+}
